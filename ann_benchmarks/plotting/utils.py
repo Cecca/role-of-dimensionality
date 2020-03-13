30f4ad9b370b09bad7934f4d1044a47039892c13
Matteo Ceccarello <mcec@itu.dk>
2020-03-13 18:34:18 +0100

    When exporting data, compute and include dimensionality metrics

--- ann_benchmarks/plotting/utils.py
+++ ann_benchmarks/plotting/utils.py
@@ -1,10 +1,115 @@
 from __future__ import absolute_import
 
+import random
 import numpy as np
 import pandas as pd
+from pandas.api.types import CategoricalDtype
 import os, itertools, json, numpy, pickle
 from ann_benchmarks.plotting.metrics import all_metrics as metrics
 import matplotlib.pyplot as plt
+import re
+from scipy.spatial.distance import cdist
+import math
+
+def compute_expansion(all_distances, k):
+    def compute_single(distances):
+        distances.sort()
+        return distances[2*k] / distances[k]
+    print("Computing the Expansion")
+    return numpy.array([
+        compute_single(ds) for ds in all_distances
+    ])
+
+def compute_lid(all_distances):
+    def compute_single(distances):
+        distances.sort()
+        w = distances[-1]
+        half_w = 0.5 * w
+        s = 0.0
+        valid = 0
+        for v in distances:
+            if v > 1e-5:
+                if v < half_w:
+                    s += numpy.log(v / w)
+                else:
+                    s += numpy.log1p((v - w) / w)
+                valid += 1
+        return -valid / s
+
+    print("Computing the Local Intrinsic Dimensionality")
+    return numpy.array([
+        compute_single(ds) for ds in all_distances
+    ])
+
+def compute_lid_10(all_distances):
+    def compute_single(distances):
+        distances.sort()
+        w = distances[9]
+        half_w = 0.5 * w
+        s = 0.0
+        valid = 0
+        for v in distances[:10]:
+            if v > 1e-5:
+                if v < half_w:
+                    s += numpy.log(v / w)
+                else:
+                    s += numpy.log1p((v - w) / w)
+                valid += 1
+        return -valid / s
+
+    print("Computing the Local Intrinsic Dimensionality (k=10)")
+    return numpy.array([
+        compute_single(ds) for ds in all_distances
+    ])
+
+def compute_rc(queries, dataset, distances, k, distance_type, n_samples=3000):
+    print("Computing the Local Relative Contrast")
+    samples = numpy.array([random.choice(dataset) 
+                           for _ in range(n_samples)])
+    
+    if 'euclidean' in distance_type:
+        avg = numpy.mean(numpy.transpose(
+            cdist(samples, queries, 'euclidean')), axis=1)
+    if 'angular' in distance_type:
+        avg = numpy.median(numpy.transpose(
+            cdist(samples, queries, 'cosine')), axis=1)
+
+    estimates = []
+    for i in range(len(queries)):
+        for j in range(10, 100):
+            if distances[i][j] > 1e-6:
+                dist = distances[i][j]
+                break
+        estimates.append(avg[i] / dist)
+    return numpy.array(estimates)
+
+def get_dimensionality_measures(dataset, distance_type):
+    if 'dimensionality_measures' not in dataset:
+        group = dataset.create_group('dimensionality_measures')
+        # measures = {
+        #     'lrc' : compute_rc(dataset['test'], 
+        #                        dataset['train'],
+        #                        dataset['distances'],
+        #                        10,
+        #                        distance_type),
+        #     'lid': compute_lid(dataset['distances']),
+        #     'lid10': compute_lid_10(dataset['distances']),
+        #     'expansion': compute_expansion(dataset['distances'], 10)
+        # }
+    if 'lrc' not in dataset['dimensionality_measures']:
+        dataset['dimensionality_measures']['lrc'] = compute_rc(dataset['test'], 
+                               dataset['train'],
+                               dataset['distances'],
+                               10,
+                               distance_type)
+    if 'lid' not in dataset['dimensionality_measures']:
+        dataset['dimensionality_measures']['lid'] = compute_lid(dataset['distances'])
+    if 'lid10' not in dataset['dimensionality_measures']:
+        dataset['dimensionality_measures']['lid10'] = compute_lid_10(dataset['distances'])
+    if 'expansion' not in dataset['dimensionality_measures']:
+        dataset['dimensionality_measures']['expansion'] = compute_expansion(dataset['distances'], 10)
+
+    return dataset['dimensionality_measures']
 
 
 def get_or_create_metrics(run):
@@ -59,24 +164,58 @@ def compute_metrics(dataset, res, metric_1, metric_2, recompute=False):
 
     return all_results
 
+difficulty_cat = pd.CategoricalDtype(['hard', 'middle', 'easy', 'diverse'])
+difficulty_type_cat = pd.CategoricalDtype(['lid', 'expansion', 'lrc'])
+algo_map = {
+    'annoy' : 'Annoy',
+    'faiss-ivf' : 'IVF',
+    'hnsw(faiss)' : 'HNSW',
+    'NGT-onng' : 'ONNG',
+    'puffinn' : "PUFFINN"
+}
+algo_cat = pd.CategoricalDtype(algo_map.values())
+dataset_map = {
+    "glove-2m-300-angular": "GLOVE-2M" ,
+    "gnews-300-angular": "GNEWS" ,
+    "glove-100-angular": "GLOVE" ,
+    "sift-128-euclidean": "SIFT" ,
+    "fashion-mnist-784-euclidean": "Fashion-MNIST" ,
+    "mnist-784-euclidean": "MNIST" 
+}
+dataset_cat = pd.CategoricalDtype(dataset_map.values())
 
-def compute_metrics_all_runs(true_nn_distances, res, recompute=False):
+def compute_metrics_all_runs(dataset, res, recompute=False):
+    true_nn_distances=list(dataset['distances'])
     for i, (properties, run) in enumerate(res):
         algo = properties['algo']
         algo_name = properties['name']
         # cache distances to avoid access to hdf5 file
-        print('Load distances and times')
+        # print('Load distances and times')
         run_distances = np.array(run['distances'])
         query_times = np.array(run['times'])
-        print('... done')
+        # print('... done')
         if recompute and 'metrics' in run:
             print('Recomputing metrics, clearing cache')
             del run['metrics']
         metrics_cache = get_or_create_metrics(run)
+        
+        dataset = properties['dataset']
+        if 'expansion' in dataset:
+            difficulty_type = 'expansion'
+        elif 'lrc' in dataset:
+            difficulty_type = "lrc"
+        else:
+            difficulty_type = "lid"
+        difficulty = re.findall("hard|middle|easy|diverse", dataset)[0]
+        dataset = re.sub("-(hard|middle|easy|diverse)", "", dataset)
+        dataset = re.sub("-(expansion|lrc|lid)", "", dataset)
+
         run_result = {
-            'algorithm': algo,
+            'algorithm': algo_map[algo],
             'parameters': algo_name,
-            'dataset': properties['dataset'],
+            'dataset': dataset_map[dataset],
+            'difficulty': difficulty,
+            'difficulty_type': difficulty_type,
             'count': properties['count']
         }
         for name, metric in metrics.items():
@@ -85,7 +224,9 @@ def compute_metrics_all_runs(true_nn_distances, res, recompute=False):
         yield run_result
 
 
-def run_to_dataframe(true_nn_distances, run, properties, recompute=False):
+def run_to_dataframe(data, run, properties, recompute=False):
+    true_nn_distances = data['distances']
+    k = len(run['distances'][0])
     algo = properties["algo"]
     algo_name = properties["name"]
     dataset = properties['dataset']
@@ -100,13 +241,37 @@ def run_to_dataframe(true_nn_distances, run, properties, recompute=False):
     metrics['k-nn']['function'](true_nn_distances, run_distances, query_times, metrics_cache, run.attrs)
     recalls = metrics_cache['knn']['recalls']
     df = pd.DataFrame({
-        'recall': recalls,
+        'recall': numpy.array(recalls) / k,
         'query_time': query_times,
     })
+    if 'expansion' in dataset:
+        difficulty_type = 'expansion'
+    elif 'lrc' in dataset:
+        difficulty_type = "lrc"
+    else:
+        difficulty_type = "lid"
+
+    difficulty = re.findall("hard|middle|easy|diverse", dataset)[0]
+    distance_type = re.findall("angular|euclidean", dataset)[0]
+    dimensionality_measures = get_dimensionality_measures(
+        data, distance_type)
+
+    dataset = re.sub("-(hard|middle|easy|diverse)", "", dataset)
+    dataset = re.sub("-(expansion|lrc|lid)", "", dataset)
+
     df['queries_per_second'] = 1.0/df['query_time']
-    df['dataset'] = dataset
-    df['algo'] = algo
-    df['params'] = algo_name
+    df['lrc'] = dimensionality_measures['lrc']
+    df['lid'] = dimensionality_measures['lid']
+    df['expansion'] = dimensionality_measures['expansion']
+    df['dataset'] = dataset_map[dataset]
+    df['dataset'] = df['dataset'].astype(dataset_cat)
+    df['algorithm'] = algo_map[algo]
+    df['algorithm'] = df['algorithm'].astype(algo_cat)
+    df['parameters'] = algo_name
+    df['difficulty_type'] = difficulty_type
+    df['difficulty_type'] = df['difficulty_type'].astype(difficulty_type_cat)
+    df['difficulty'] = difficulty
+    df['difficulty'] = df['difficulty'].astype(difficulty_cat)
     return df
 
 
@@ -114,7 +279,10 @@ def runs_to_dataframe(true_nn_distances, res, recompute=False):
     dfs = []
     for i, (properties, run) in enumerate(res):
         dfs.append(run_to_dataframe(true_nn_distances, run, properties, recompute))
-    return pd.concat(dfs)
+    if len(dfs) > 0:
+        return pd.concat(dfs)
+    else:
+        return None
 
 
 def compute_all_metrics(true_nn_distances, run, properties, recompute=False):
--- to_csv.py
+++ to_csv.py
@@ -6,6 +6,8 @@ mpl.use('Agg')
 import matplotlib.pyplot as plt
 import argparse
 import bz2
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 from ann_benchmarks.datasets import get_dataset
 from ann_benchmarks.algorithms.definitions import get_definitions
@@ -14,22 +16,49 @@ from ann_benchmarks.plotting.utils  import get_plot_label, compute_metrics_all_r
 from ann_benchmarks.results import store_results, load_all_results, get_unique_algorithms, get_algorithm_name
 
 datasets = [
+    # LRC
+    'fashion-mnist-784-euclidean-diverse-lrc',
+    'fashion-mnist-784-euclidean-easy-lrc',
+    'fashion-mnist-784-euclidean-hard-lrc',
+    'fashion-mnist-784-euclidean-middle-lrc',
+    'glove-100-angular-diverse-lrc',
+    'glove-100-angular-easy-lrc',
+    'glove-100-angular-hard-lrc',
+    'glove-100-angular-middle-lrc',
+    'glove-2m-300-angular-diverse-lrc',
+    'glove-2m-300-angular-easy-lrc',
+    'glove-2m-300-angular-hard-lrc',
+    'glove-2m-300-angular-middle-lrc',
+    'gnews-300-angular-diverse-lrc',
+    'gnews-300-angular-easy-lrc',
+    'gnews-300-angular-hard-lrc',
+    'gnews-300-angular-middle-lrc',
+    'mnist-784-euclidean-diverse-lrc',
+    'mnist-784-euclidean-easy-lrc',
+    'mnist-784-euclidean-hard-lrc',
+    'mnist-784-euclidean-middle-lrc',
+    'sift-128-euclidean-diverse-lrc',
+    'sift-128-euclidean-easy-lrc',
+    'sift-128-euclidean-hard-lrc',
+    'sift-128-euclidean-middle-lrc',
+    # Expansion
     'fashion-mnist-784-euclidean-diverse-expansion',
-    'mnist-784-euclidean-diverse-expansion',
-    'glove-100-angular-diverse-expansion',
-    'glove-2m-300-angular-diverse-expansion',
-    'gnews-300-angular-diverse-expansion',
-    'mnist-784-euclidean-diverse-expansion',
-    'sift-128-euclidean-diverse-expansion',
     'fashion-mnist-784-euclidean-easy-expansion',
     'fashion-mnist-784-euclidean-hard-expansion',
     'fashion-mnist-784-euclidean-middle-expansion',
-    'glove-2m-300-angular-easy-expansion',
-    'glove-2m-300-angular-middle-expansion',
-    'glove-2m-300-angular-hard-expansion',
+    'glove-100-angular-diverse-expansion',
     'glove-100-angular-easy-expansion',
     'glove-100-angular-hard-expansion',
     'glove-100-angular-middle-expansion',
+    'glove-2m-300-angular-diverse-expansion',
+    'glove-2m-300-angular-easy-expansion',
+    'glove-2m-300-angular-hard-expansion',
+    'glove-2m-300-angular-middle-expansion',
+    'gnews-300-angular-diverse-expansion',
+    'gnews-300-angular-easy-expansion',
+    'gnews-300-angular-hard-expansion',
+    'gnews-300-angular-middle-expansion',
+    'mnist-784-euclidean-diverse-expansion',
     'mnist-784-euclidean-easy-expansion',
     'mnist-784-euclidean-hard-expansion',
     'mnist-784-euclidean-middle-expansion',
@@ -37,29 +66,31 @@ datasets = [
     'sift-128-euclidean-easy-expansion',
     'sift-128-euclidean-hard-expansion',
     'sift-128-euclidean-middle-expansion',
-    'fashion-mnist-784-euclidean-diverse',
-    'mnist-784-euclidean-diverse',
-    'glove-100-angular-diverse',
-    'glove-2m-300-angular-diverse',
-    'gnews-300-angular-diverse',
-    'mnist-784-euclidean-diverse',
-    'sift-128-euclidean-diverse',
-    'fashion-mnist-784-euclidean-easy',
-    'fashion-mnist-784-euclidean-hard',
-    'fashion-mnist-784-euclidean-middle',
-    'glove-2m-300-angular-easy',
-    'glove-2m-300-angular-middle',
-    'glove-2m-300-angular-hard',
-    'glove-100-angular-easy',
-    'glove-100-angular-hard',
-    'glove-100-angular-middle',
-    'mnist-784-euclidean-easy',
-    'mnist-784-euclidean-hard',
-    'mnist-784-euclidean-middle',
-    'sift-128-euclidean-diverse',
-    'sift-128-euclidean-easy',
-    'sift-128-euclidean-hard',
-    'sift-128-euclidean-middle',
+    # LID
+    'fashion-mnist-784-euclidean-diverse-lid',
+    'fashion-mnist-784-euclidean-easy-lid',
+    'fashion-mnist-784-euclidean-hard-lid',
+    'fashion-mnist-784-euclidean-middle-lid',
+    'glove-100-angular-diverse-lid',
+    'glove-100-angular-easy-lid',
+    'glove-100-angular-hard-lid',
+    'glove-100-angular-middle-lid',
+    'glove-2m-300-angular-diverse-lid',
+    'glove-2m-300-angular-easy-lid',
+    'glove-2m-300-angular-hard-lid',
+    'glove-2m-300-angular-middle-lid',
+    'gnews-300-angular-diverse-lid',
+    'gnews-300-angular-easy-lid',
+    'gnews-300-angular-hard-lid',
+    'gnews-300-angular-middle-lid',
+    'mnist-784-euclidean-diverse-lid',
+    'mnist-784-euclidean-easy-lid',
+    'mnist-784-euclidean-hard-lid',
+    'mnist-784-euclidean-middle-lid',
+    'sift-128-euclidean-diverse-lid',
+    'sift-128-euclidean-easy-lid',
+    'sift-128-euclidean-hard-lid',
+    'sift-128-euclidean-middle-lid'
 ]
 
 # datasets = [p[:-len('.hdf5')] for p in os.listdir('data') if p.endswith('.hdf5')]
@@ -96,17 +127,36 @@ if __name__ == "__main__":
 
     count = int(args.count)
     dfs = []
+
+    is_first = True
+    pqwriter = None
     for dataset_name in datasets:
         print("Looking at dataset", dataset_name)
         dataset = get_dataset(dataset_name)
         unique_algorithms = get_unique_algorithms()
         results = load_all_results(dataset_name, count, True, args.batch)
         if args.detail:
-            df = runs_to_dataframe(list(dataset["distances"]), results, args.recompute)
-            dfs.append(df)
+            # df = runs_to_dataframe(list(dataset["distances"]), results, args.recompute)
+            df = runs_to_dataframe(dataset, results, args.recompute)
+            if df is not None:
+                table = pa.Table.from_pandas(df)
+                if is_first:
+                    pqwriter = pq.ParquetWriter(args.output, table.schema)
+                    is_first = False
+                pqwriter.write_table(table)
+            else:
+                print("WARNING: problems getting results for", dataset_name)
+            ## Directly write the data frame, appending to the file
+            #df.to_csv(args.output, mode="w" if is_first else "a",
+            #          header=is_first, index=False
+            #          #, compression='bz2'
+            #          )
         else:
-            results = compute_metrics_all_runs(list(dataset["distances"]), results, args.recompute)
+            results = compute_metrics_all_runs(dataset, results, args.recompute)
             dfs.append(pd.DataFrame(results))
-    data = pd.concat(dfs)
-    data.to_csv(args.output, index=False, compression='bz2')
+    if len(dfs) > 0:
+        data = pd.concat(dfs)
+        data.to_csv(args.output, index=False, compression='bz2')
+    if pqwriter is not None:
+        pqwriter.close()
 
