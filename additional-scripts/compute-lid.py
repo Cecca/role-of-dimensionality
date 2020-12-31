13b34408fdf648dcafacb9a6ce14ba6fe7376011
Matteo Ceccarello <mcec@itu.dk>
2020-12-31 08:36:28 +0100

    Optimize compute-lid.py

--- additional-scripts/compute-lid.py
+++ additional-scripts/compute-lid.py
@@ -20,15 +20,29 @@ for i, vec in enumerate(distances):
     vec.sort()
     w = vec[min(len(vec) - 1, k)]
     half_w = 0.5 * w
-    s = 0.0
-    valid = 0
-    for v in vec[:k+1]:
-        if v > 1e-5:
-            if v < half_w:
-                s += math.log(v / w)
-            else:
-                s += numpy.log1p((v - w) / w)
-            valid += 1
+
+    vec = vec[:k+1]
+    vec = vec[vec > 1e-5]
+
+    # Use numpy vector operations to improve efficiency.
+    # Results are the same up the the 6th decimal position
+    # compared to iteration
+
+    small = vec[vec < half_w]
+    large = vec[vec >= half_w]
+
+    s = numpy.log(small / w).sum() + numpy.log1p((large - w) / w).sum()
+    valid = small.size + large.size
+
+    # s = 0.0
+    # valid = 0
+    # for v in vec[:k+1]:
+    #     if v > 1e-5:
+    #         if v < half_w:
+    #             s += math.log(v / w)
+    #         else:
+    #             s += numpy.log1p((v - w) / w)
+    #         valid += 1
     estimates.append(-valid / s)
 
 for i,e in enumerate(estimates):
